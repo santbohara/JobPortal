8c11ed2d0653618ff4158c601494106cc9bddf85
santbohara <santbohara@gmail.com>
2022-07-16 18:47:37 +0545

    Add Job Optimized

---
+++ JobPortal/Areas/Jobs/Controllers/AddController.cs
@@ -0,0 +1,35 @@
+﻿using JobPortal.Data;
+using JobPortal.Areas.Jobs.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Areas.Jobs.Controllers
+{
+    [Area("Jobs")]
+    [Authorize]
+    public class AddController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AddController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            AddViewModel lists = new()
+            {
+                JobCategory     = _db.JobCategory.Where(c => c.IsActive == true).ToList(),
+                JobQualification = _db.JobQualification.Where(c => c.IsActive == true).ToList(),
+                JobType         = _db.JobType.Where(c => c.IsActive == true).ToList(),
+                SalaryType      = _db.SalaryType.Where(c => c.IsActive == true).ToList(),
+                SalaryRange     = _db.SalaryRange.Where(c => c.IsActive == true).ToList(),
+                JobExperience   = _db.JobExperience.Where(c => c.IsActive == true).ToList(),
+                JobShift        = _db.JobShift.Where(c => c.IsActive == true).ToList(),
+                JobLevel        = _db.JobLevel.Where(c => c.IsActive == true).ToList(),
+            };
+
+            return View(lists);
+        }
+    }
+}
---
+++ JobPortal/Areas/Jobs/ViewModel/AddViewModel.cs
@@ -0,0 +1,16 @@
+﻿using JobPortal.Areas.Config.Models;
+
+namespace JobPortal.Areas.Jobs.ViewModel
+{
+    public class AddViewModel
+    {
+        public List<JobCategory> JobCategory { get; set; }
+        public List<JobQualification> JobQualification { get; set; }
+        public List<JobType> JobType { get; set; }
+        public List<SalaryType> SalaryType { get; set; }
+        public List<SalaryRange> SalaryRange { get; set; }
+        public List<JobExperience> JobExperience { get; set; }
+        public List<JobShift> JobShift { get; set; }
+        public List<JobLevel> JobLevel { get; set; }
+    }
+}
